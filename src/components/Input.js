94f91673d8eb4c9cb72f875e914edfc9a494698e
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2021-02-09 16:06:29 +0530

    import error. fixes #1

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 
-import saveTodo from "../features/todoSlice";
+import { saveTodo } from "../features/todoSlice";
 
 import "./Input.css";
 
